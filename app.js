575b6019ab7298fe0badf71b1c603fc3c4c3cd19
hussainmaz <mazhussain2020@gmail.com>
2017-04-09 11:25:15 +0100

    added api book route to app.js

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var express = require('express');
-
 var app = express();
 var port = process.env.PORT || 3000;
+var bookRouter = express.Router();
 
+//api/books/get
+bookRouter.route('/Books')
+    .get(function (req, res) {
+        var responseJson = { hello: "This is my api" };
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
+
+// standard api function
 app.get('/', function (req, res) {
     res.send('welcome to my API running under gulp!!!!');
 });
 
+
+//api port...
 app.listen(port, function () {
     console.log("Gulp is running my app on PORT:" + port);
 });
