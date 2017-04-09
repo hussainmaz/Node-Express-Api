69d51adf456d8c63a8107f791243bf43e85df499
hussainmaz <mazhussain2020@gmail.com>
2017-04-09 18:28:07 +0100

    Added api Post operation and refactored out routes

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
-var express = require('express');
-var mongoose = require('mongoose');
-
-var db = mongoose.connect('mongodb://localhost/bookAPI');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 var Book = require('./models/bookModel');
+var bookRouter = require('./Routes/bookRoutes')(Book);
 
+//mongo db
+var db = mongoose.connect('mongodb://localhost/bookAPI');
 
+//app initialisation
 var app = express();
 var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-
-
-
-var bookRouter = express.Router();
-//api/books/get
-bookRouter.route('/Books')
-    .get(function (req, res) {
-
-        var query = {};
-        if (req.query.genre) {
-            query.genre = req.query.genre;
-        }
-        console.log(query);
-
-        Book.find(query, function (err, books) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.json(books)
-            }
-        })
-    });
-
-// use book router..Monyou
-app.use('/api', bookRouter);
-
-// standard api function
 app.get('/', function (req, res) {
-    res.send('welcome to my API running under gulp!!!!');
+    res.send('Started Running Node Express Api!....');
 });
 
-
-//api port...
 app.listen(port, function () {
-    console.log("Gulp is running my app on PORT:" + port);
+    console.log('Running api on port: ' + port);
 });
+
+//add book router to app
+app.use('/api/books', bookRouter);
+
