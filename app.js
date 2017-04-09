747c165921a071b2456e238b60609fe77277075d
hussainmaz <mazhussain2020@gmail.com>
2017-04-09 11:08:31 +0100

    updated app.js to run under gulp with nodemon

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.send('welcome to my API!');
+    res.send('welcome to my API running under gulp!!!!');
 });
 
 app.listen(port, function () {
-    console.log("Running on PORT:" + port);
+    console.log("Gulp is running my app on PORT:" + port);
 });
